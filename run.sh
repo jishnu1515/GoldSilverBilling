b0eaa18060655570c9bfbd69690004445108d43c
PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>
2024-12-06 11:16:41 +0530

    Delete run.sh
    Signed-off-by: PSP VU21CSEN0101025 <93901037+jishnu1515@users.noreply.github.com>

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir=$(dirname $0)
-cd $script_dir
-# Stop and remove any existing containers
-docker-compose down
-
-# Build the Docker image and start the containers
-docker-compose up --build
